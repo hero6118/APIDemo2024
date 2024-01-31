641b6286acd259377eefd3a47fb71c0f2ce989ad
thuan <thuannguyenTHCST4@gmail.com>
2024-01-31 20:57:42 +0700

    update Authentication

--- DemoAPI_List/Data/AppDbContext.cs
+++ DemoAPI_List/Data/AppDbContext.cs
@@ -1,17 +1,23 @@
 ﻿using DemoAPI_List.Models;
+using DemoAPI_List.Models.Authentication;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 
 namespace DemoAPI_List.Data
 {
     
-        public class AppDbContext : DbContext
-        {
+        public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
             public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
             {
             }
+            protected override void OnModelCreating(ModelBuilder builder)
+            {
+                base.OnModelCreating(builder);
+            }
 
-            public DbSet<Product> Products { get; set; }
+        public DbSet<Product> Products { get; set; }
         }
     
 }
---
+++ DemoAPI_List/Models/Response/Response.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoAPI_List.Models.Response
+{
+    
+        public class Responsed
+        {
+            public string Status { get; set; }
+            public string Message { get; set; }
+        }
+    
+}
